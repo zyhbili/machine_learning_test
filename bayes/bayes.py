6b517d5f6dd5e814102db361e1b9295ffabeea66
zyhbili <1372241206@qq.com>
2018-12-20 09:31:21 +0800

    bayes

--- bayes/bayes.py
+++ bayes/bayes.py
@@ -3,6 +3,8 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from sklearn.metrics import roc_curve, auc
+
 
 #txt
 # def loadDataSet(fileName):
@@ -59,85 +61,107 @@ def classify(trainData, labels, features):
     features_y = max(F, key=F.get)  # 概率最大值对应的类别
     return features_y
 
-trainData, labels=getTrainSet('studentdataset.csv')
-# # print(classify(trainData, labels,[180,50,40]))
-# m=np.mat(trainData)
-# print(m.shape)
-# print(m[:2,:2])
-# a=np.ones()
-# print(np.ones(2))
-# print(np.sum(m[:2,:2]))
-#
-# class kernal:
-#     def gaussian(self,x):
-#     pass
-#
-#
-# def parzen(train_mat,hn,X):
-#     pass
 
 
-def m_pdf(train_array,xfc,average):
-
-    d=train_array.shape[1]
-
-    temp=1/((2*math.pi)**(d/2)*(np.linalg.det(xfc)**0.5))
-
-    t=-1/2*(train_array-average).T*xfc.I*(train_array-average)
-
-    return temp*np.exp(t)
-
-
-x=[[180,70]]
-x=np.mat(x)
-
-# a = np.expand_dims(trainData, axis=1)
-# print(a)
-
-# b=np.expand_dims(x, axis=0)
-# print("b",b)
-# print(b-a)
-# print(trainData-x)
-# b = np.all(np.less(np.abs(trainData-x), 1/2), axis=-1)
-# print(b)
-# average= sum(trainData)/trainData.shape[0]
-# average=average[0:2]
-# xfc=np.cov(trainData.T)
-# print(xfc[0:100])
-#
-# print(m_pdf(x,np.mat(xfc),np.mat(average).T))
-
-
-
-def gaussian_window(train_mat, mu, h):
-    N, d = train_mat.shape
-    a = (train_mat - mu) / h
-    b=[]
-    for i in a:
-        b.append(1/np.sqrt(np.pi * 2)*np.exp(-0.5*int(i*i.T)))
-    kn=sum(b)
-    Vn=h**d
-    return kn / (N * Vn)
-
-def hypercube_kernel(train_mat, mu, h):
-    N, d = train_mat.shape
-    a = (train_mat - mu) / h
-    b = np.all(np.less(np.abs(a), 1 / 2), axis=1)
-    kn = np.sum(b.astype(int))
-    Vn=h**d
-    return kn / (N * Vn)
 
 
 
 dataSet = pd.read_csv('studentdataset.csv')
+
 dataSetNP = np.array(dataSet)
+np.random.shuffle(dataSetNP)
+testData=dataSetNP[0:500]
+boy_dataNP = []
+girl_dataNP = []
+for x in dataSetNP:
+    if x[3] == 1:
+        boy_dataNP.append(x)
+    else:
+        girl_dataNP.append(x)
+boy_dataNP = np.array(boy_dataNP)
+girl_dataNP = np.array(girl_dataNP)
+# print(boy_dataNP.shape[0])
+# print(girl_dataNP.shape[0])
+np.random.shuffle(boy_dataNP)
+np.random.shuffle(girl_dataNP)
+
+dataSetNP = np.vstack((boy_dataNP[0:250], girl_dataNP[0:250]))
+
 trains = dataSetNP[:, 0:dataSetNP.shape[1] - 1]
 labels = dataSetNP[:, dataSetNP.shape[1] - 1]
 labels = list(labels)
 Px = {}
+# 先验概率控制为0.5对0.5#
 Px[0] = labels.count(0) / len(labels)
 Px[1] = labels.count(1) / len(labels)
-print(Px[0], Px[1])
+boy_sum = 0
+girl_sum = 0
+for x in dataSetNP:
+    if x[3] == 1:
+        boy_sum += x[0]
+    else:
+        girl_sum += x[0]
+boy_average = boy_sum / labels.count(1)
+girl_average = girl_sum / labels.count(0)
+# print(boy_average, girl_average)
+boy_sig = 0
+girl_sig = 0
+for x in dataSetNP:
+    if x[3] == 1:
+        boy_sig += (x[0] - boy_average) ** 2
+    else:
+        girl_sig += (x[0] - girl_average) ** 2
+boy_sig = (boy_sig / labels.count(1)) ** 0.5
+girl_sig = (girl_sig / labels.count(0)) ** 0.5
+
+def bayes_2(test_sample):
+    test_sample = np.mat(test_sample).T
+    pb_test_sample = m_pdf(test_sample, np.mat(xfcBoy), np.mat(averageBoy).T)
+    pg_test_sample = m_pdf(test_sample, np.mat(xfcGirl), np.mat(averageGirl).T)
+    ppb_test_sample = pb_test_sample * Px[1] / (pb_test_sample * Px[1] + pg_test_sample * Px[0])
+    ppg_test_sample = pg_test_sample * Px[0] / (pb_test_sample * Px[1] + pg_test_sample * Px[0])
+    return (ppb_test_sample - ppg_test_sample)
+
+def bayes_1_draw():
+    x = np.linspace(140, 190, 50)
+    by = np.exp(-(x - boy_average) ** 2 / (2 * (boy_sig ** 2))) / (math.sqrt(2 * math.pi) * boy_sig)
+    gy = np.exp(-(x - girl_average) ** 2 / (2 * (girl_sig ** 2))) / (math.sqrt(2 * math.pi) * girl_sig)
+    byy = by * Px[1] / (by * Px[1] + gy * Px[0])
+    gyy = gy * Px[0] / (by * Px[1] + gy * Px[0])
+    plt.plot(x, by, 'r', label='boy')
+    plt.plot(x, gy, 'b', label='girl')
+    plt.legend(loc='center right')
+    plt.xlabel('Height')
+    plt.ylabel('Probability Density')
+    plt.title('Class conditional probability')
+    plt.show()
+
+    plt.plot(x, byy, 'r', label='boy')
+    plt.plot(x, gyy, 'b', label='girl')
+    plt.legend(loc='center right')
+    plt.xlabel('Height')
+    plt.ylabel('Probability')
+    plt.title('Posterior probability')
+    plt.show()
+
+
+def bayes_height(test_sample):
+    pb_test_sample = np.exp(-(test_sample - boy_average) ** 2 / (2 * (boy_sig ** 2))) / (
+            math.sqrt(2 * math.pi) * boy_sig)
+    pg_test_sample = np.exp(-(test_sample - girl_average) ** 2 / (2 * (girl_sig ** 2))) / (
+            math.sqrt(2 * math.pi) * girl_sig)
+    ppb_test_sample = pb_test_sample * Px[1] / (pb_test_sample * Px[1] + pg_test_sample * Px[0])
+    ppg_test_sample = pg_test_sample * Px[0] / (pb_test_sample * Px[1] + pg_test_sample * Px[0])
+    return (ppb_test_sample - ppg_test_sample)
+
+
+def m_pdf(train_array, xfc, average):
+    d = train_array.shape[1]
+    temp = 1 / ((2 * math.pi) ** (d / 2) * (np.linalg.det(xfc) ** 0.5))
+    t = -1 / 2 * (train_array - average).T * xfc.I * (train_array - average)
+    return float(temp * np.exp(t))
+
+
 boy = []
 girl = []
 for x in dataSetNP:
@@ -149,56 +173,120 @@ boy = np.array(boy)
 girl = np.array(girl)
 trainDataBoy = boy[:, 0:2]
 trainDataGirl = girl[:, 0:2]
-trainData=trains[:,0:2]
+averageBoy = sum(trainDataBoy) / trainDataBoy.shape[0]
+averageBoy = averageBoy[0:2]
+# print(averageBoy)
+xfcBoy = np.cov(trainDataBoy.T)
 
-fig = plt.figure()
-ax = Axes3D(fig)
-X = np.arange(140, 200, 1)
-Y = np.arange(30, 90, 1)
-xx = np.hstack((X, Y))
-XX, YY = np.meshgrid(X, Y)
-
-ZB = []
-ZG = []
-for i in X:
-    bbb = []
-    ggg = []
-    for j in Y:
-        xxxx = np.mat((i, j))
-        bbb.append(hypercube_kernel(trainDataBoy,xxxx,10))
-        ggg.append(hypercube_kernel(trainDataGirl,xxxx,10))
-    ZB.append(bbb)
-    ZG.append(ggg)
-
-ZB = np.array(ZB)
-ZG = np.array(ZG)
-ax.plot_surface(XX, YY, ZB, rstride=1, cstride=1, cmap='rainbow')
-ax.plot_surface(XX, YY, ZG, rstride=1, cstride=1, cmap='rainbow')
-plt.show()
+averageGirl = sum(trainDataGirl) / trainDataGirl.shape[0]
+averageGirl = averageGirl[0:2]
+# print(averageGirl)
+xfcGirl = np.cov(trainDataGirl.T)
 
 
-# print(gaussian_window(trainData,x,8))
-# print(hypercube_kernel(trainData,x,1))
+def bayes_2_draw():
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    X = np.arange(140, 200, 1)
+    Y = np.arange(30, 90, 1)
+    XX, YY = np.meshgrid(X, Y)
+    ZB = []
+    ZG = []
+    for i in X:
+        bbb = []
+        ggg = []
+        for j in Y:
+            xxxx = np.mat((i, j)).T
+            bbb.append(m_pdf(xxxx, np.mat(xfcBoy), np.mat(averageBoy).T))
+            ggg.append(m_pdf(xxxx, np.mat(xfcGirl), np.mat(averageGirl).T))
+        ZB.append(bbb)
+        ZG.append(ggg)
+    ZB = np.array(ZB)
+    ZG = np.array(ZG)
+    ax.plot_surface(XX, YY, ZB, rstride=1, cstride=1, cmap='Blues')
+    ax.plot_surface(XX, YY, ZG, rstride=1, cstride=1, cmap='Oranges')
+    plt.show()
 
-def judge(xxxx,h):
-    g1=hypercube_kernel(trainDataBoy,xxxx,h)*Px[1]
-    g0=hypercube_kernel(trainDataGirl, xxxx, h)*Px[0]
-    if(g1>g0):
-        return 1
-    return 0
-success_num=0
-num=len(labels)
-print(num)
-ratio=[]
-for h in range(1,25):
-    for i in range(num):
-        # print(trainData[i])
-        # print(i)
-        # print(hypercube_kernel(trainDataBoy,trainData[i],40))
-        # print(hypercube_kernel(trainDataGirl,trainData[i],40))
-        if(labels[i]==judge(trainData[i],h)):
-            success_num+=1
-    ratio.append(success_num/num)
-    success_num=0
 
-print(ratio)
+def bayes2_test():
+    right = 0
+    print("二维（身高，体重）：")
+    real = []
+    predict = []
+    for x in testData:
+        c = bayes_2((float(x[0]), float(x[1])))
+        real.append(x[3])
+        predict.append(c)
+        if c > 0:
+            c = 1
+        else:
+            c = 0
+        if c == x[3]: right += 1
+    print(right / dataSetNP.shape[0])
+    # print(ratio)
+    fpr, tpr, thresholds = roc_curve(real, predict, pos_label=1)
+    roc_auc = auc(fpr, tpr)
+
+    plt.figure()
+    lw = 1
+    plt.figure(figsize=(5, 5))
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)  ###假正率为横坐标，真正率为纵坐标做曲线
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC')
+    plt.legend(loc="lower right")
+    plt.show()
+
+def bayes1_test():
+    TP = 0
+    FP = 0
+    FN = 0
+    TN = 0  # P:男，N:女
+    real = []
+    predict = []
+    for x in testData:
+        c = bayes_height(float(x[0]))
+        real.append(x[3])
+        predict.append(c)
+        if c > 0:
+            c = 1
+        else:
+            c = 0
+        if (x[3] == 1 and 1 == c):
+            TP += 1
+        if (x[3] == 1 and 0 == c):
+            FP += 1
+        if (x[3] == 0 and 0 == c):
+            TN += 1
+        if (x[3] == 0 and 0 == c):
+            FN += 1
+    ratio = (TP + TN) / dataSetNP.shape[0]
+    print("一维（身高）：")
+    # print(wrong / dataSetNP.shape[0])
+    print(ratio)
+    fpr, tpr, thresholds = roc_curve(real, predict, pos_label=1)
+    roc_auc = auc(fpr, tpr)
+
+    plt.figure()
+    lw = 1
+    plt.figure(figsize=(5, 5))
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)  ###假正率为横坐标，真正率为纵坐标做曲线
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC')
+    plt.legend(loc="lower right")
+    plt.show()
+
+
+bayes_1_draw()
+bayes_2_draw()
+bayes1_test()
+bayes2_test()
--- bayes/parzen.py
+++ bayes/parzen.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.model_selection import RepeatedKFold
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from sklearn.metrics import roc_curve, auc
 def gaussian_window(train_mat, mu, h):
     N, d = train_mat.shape
     a = (train_mat - mu) / h
@@ -22,7 +23,7 @@ def hypercube_kernel(train_mat, mu, h):
     Vn=h**d
     return kn / (N * Vn)
 
-def draw(func,h):
+def draw(func,mtype,h):
     fig = plt.figure()
     ax = Axes3D(fig)
     X = np.arange(140, 200, 1)
@@ -30,6 +31,7 @@ def draw(func,h):
     XX, YY = np.meshgrid(X, Y)
     ZB = []
     ZG = []
+    ax.set_title(mtype+" "+" h:"+str(h))
     for i in X:
         bbb = []
         ggg = []
@@ -46,12 +48,134 @@ def draw(func,h):
     ax.plot_surface(XX, YY, ZG, rstride=1, cstride=1, cmap='Oranges')
     plt.show()
 
+def drawROC(trains):
+    h=5
+    kf = RepeatedKFold(n_splits=3, n_repeats=10, random_state=0)
+    for train_index, test_index in kf.split(trains):
+        girls = [];boys = [];predict=[];real=[]
+        train_len = len(train_index)
+        for i in train_index:
+            if (labels[i] == 1):
+                boys.append(trains[i])
+            else:
+                girls.append(trains[i])
+
+        boys = np.array(boys);
+        girls = np.array(girls);
+        Px = {}
+        Px[0] = len(girls) / train_len
+        Px[1] = len(boys) / train_len
+        for i in test_index:
+            c = f(boys, girls, trains[i], h, Px[0], Px[1])
+            predict.append(c)
+            real.append(labels[i])
+        break
+    fpr, tpr, thresholds = roc_curve(real,predict,pos_label=1)
+    roc_auc = auc(fpr,tpr)
+
+    plt.figure()
+    lw = 1
+    plt.figure(figsize=(5, 5))
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='3d ROC curve (area = %0.2f)' % roc_auc)  ###假正率为横坐标，真正率为纵坐标做曲线
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC')
+    plt.legend(loc="lower right")
+    # plt.show()
+
+
+    trains=trains[:,0:2]
+    kf = RepeatedKFold(n_splits=3, n_repeats=10, random_state=0)
+    for train_index, test_index in kf.split(trains):
+        girls = [];boys = [];predict=[];real=[]
+        train_len = len(train_index)
+        for i in train_index:
+            if (labels[i] == 1):
+                boys.append(trains[i])
+            else:
+                girls.append(trains[i])
+
+        boys = np.array(boys);
+        girls = np.array(girls);
+        Px = {}
+        Px[0] = len(girls) / train_len
+        Px[1] = len(boys) / train_len
+        for i in test_index:
+            c = f(boys, girls, trains[i], h, Px[0], Px[1])
+            predict.append(c)
+            real.append(labels[i])
+        break
+    fpr, tpr, thresholds = roc_curve(real,predict,pos_label=1)
+    roc_auc = auc(fpr,tpr)
+
+    # plt.figure()
+    lw = 1
+    # plt.figure(figsize=(5, 5))
+    plt.plot(fpr, tpr, color='Blue',
+             lw=lw, label='2d ROC curve (area = %0.2f)' % roc_auc)  ###假正率为横坐标，真正率为纵坐标做曲线
+    # plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    # plt.xlim([0.0, 1.0])
+    # plt.ylim([0.0, 1.0])
+    # plt.xlabel('False Positive Rate')
+    # plt.ylabel('True Positive Rate')
+    # plt.title('ROC')
+    plt.legend(loc="lower right")
+    # plt.show()
+
+    trains = trains[:, 0:1]
+    kf = RepeatedKFold(n_splits=3, n_repeats=10, random_state=0)
+    for train_index, test_index in kf.split(trains):
+        girls = [];
+        boys = [];
+        predict = [];
+        real = []
+        train_len = len(train_index)
+        for i in train_index:
+            if (labels[i] == 1):
+                boys.append(trains[i])
+            else:
+                girls.append(trains[i])
+
+        boys = np.array(boys);
+        girls = np.array(girls);
+        Px = {}
+        Px[0] = len(girls) / train_len
+        Px[1] = len(boys) / train_len
+        for i in test_index:
+            c = f(boys, girls, trains[i], h, Px[0], Px[1])
+            predict.append(c)
+            real.append(labels[i])
+        break
+    fpr, tpr, thresholds = roc_curve(real, predict, pos_label=1)
+    roc_auc = auc(fpr, tpr)
+
+    # plt.figure()
+    lw = 1
+    # plt.figure(figsize=(5, 5))
+    plt.plot(fpr, tpr, color='Red',
+             lw=lw, label='1d ROC curve (area = %0.2f)' % roc_auc)
+    plt.legend(loc="lower right")
+
+    plt.show()
+
+
+
+
+
 def judge_1(trainDataBoy,trainDataGirl,X,h,p0,p1):
     g1=hypercube_kernel(trainDataBoy,X,h)*p1
     g0=hypercube_kernel(trainDataGirl,X,h)*p0
     if(g1>g0):
         return 1
     return 0
+def f(trainDataBoy,trainDataGirl,X,h,p0,p1):
+    g1 = hypercube_kernel(trainDataBoy, X, h) * p1
+    g0 = hypercube_kernel(trainDataGirl, X, h) * p0
+    return g1-g0
 
 def judge_2(trainDataBoy,trainDataGirl,X,h,p0,p1):
     g1=gaussian_window(trainDataBoy,X,h)*p1
@@ -59,6 +183,47 @@ def judge_2(trainDataBoy,trainDataGirl,X,h,p0,p1):
     if(g1>g0):
         return 1
     return 0
+
+
+def ten_fold_verify(h):
+    T_ratio=[]
+    kf = RepeatedKFold(n_splits=10, n_repeats=10, random_state=0)
+    for train_index, test_index in kf.split(trains):
+        # print('train_index', train_index, 'test_index', test_index)
+        girls=[];boys=[];TPR_list=[];FPR_list=[]
+        TP=0;FP=0;FN=0;TN=0#P:男，N:女
+        train_len=len(train_index)
+        for i in train_index:
+            if(labels[i]==1):
+                boys.append(trains[i])
+            else:
+                girls.append(trains[i])
+
+        boys=np.array(boys);girls=np.array(girls);
+        Px={}
+        Px[0]=len(girls)/train_len
+        Px[1]=len(boys)/train_len
+        for i in test_index:
+            c=judge_1(boys,girls,trains[i],h,Px[0],Px[1])
+
+            if(labels[i]==1 and 1==c):
+                TP+=1
+            if(labels[i]==1 and 0==c):
+                FP+=1
+            if(labels[i]==0 and 0==c):
+                TN+=1
+            if(labels[i]==0 and 1==c):
+                FN+=1
+        ratio=(TP+TN)/len(test_index)
+        TPR=TP/(TP+FN)
+        FPR=FP/(TN+FP)
+        TPR_list.append(TPR)
+        FPR_list.append(FPR)
+        T_ratio.append(ratio)
+
+    print(sum(T_ratio)/len(T_ratio))
+    return sum(T_ratio)/len(T_ratio)
+
 
 dataSet = pd.read_csv('studentdataset.csv')
 dataSetNP = np.array(dataSet)
@@ -82,49 +247,24 @@ girls = np.array(girls)
 trainDataBoy = boys[:, 0:2]
 trainDataGirl = girls[:, 0:2]
 trainData=trains[:,0:2]
-h=5
-draw(hypercube_kernel,h)
-T_ratio=[]
-kf = RepeatedKFold(n_splits=10, n_repeats=10, random_state=0)
-for train_index, test_index in kf.split(trains):
-    # print('train_index', train_index, 'test_index', test_index)
-    girls=[];boys=[]
-    TP=0;FP=0;FN=0;TN=0#P:男，N:女
-    train_len=len(train_index)
-    for i in train_index:
-        if(labels[i]==1):
-            boys.append(trains[i])
-        else:
-            girls.append(trains[i])
+# draw(hypercube_kernel,"hypercube_kernel",1)
+acc=[]
+for h in range(1,30):
+    acc.append(ten_fold_verify(h))
+# drawROC(trains)
+temp=[i for i in range(1,30)]
+plt.figure()
+plt.plot(temp,acc,'r',label='acc')
+plt.legend(loc='center right')
+plt.xlabel('h-range')
+plt.ylabel('acc')
+plt.title('acc-h')
+plt.show()
 
-    boys=np.array(boys);girls=np.array(girls);
-    Px={}
-    Px[0]=len(girls)/train_len
-    Px[1]=len(boys)/train_len
-    for i in test_index:
-        c=judge_1(boys,girls,trains[i],h,Px[0],Px[1])
-        if(labels[i]==1 and 1==c):
-            TP+=1
-        if(labels[i]==1 and 0==c):
-            FP+=1
-        if(labels[i]==0 and 0==c):
-            TN+=1
-        if(labels[i]==0 and 0==c):
-            FN+=1
-    ratio=(TP+TN)/len(test_index)
-    T_ratio.append(ratio)
-print(sum(T_ratio)/len(T_ratio))
+
+
+# draw(gaussian_window,"gaussian",1)
 
 
 
 
-
-
-
-# for h in range(1,25):
-#     for i in range(num):
-#         if(labels[i]==judge(trainData[i],h)):
-#             success_num+=1
-#     ratio.append(success_num/num)
-#     success_num=0
-# print(ratio)
